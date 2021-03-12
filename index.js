bb7d116350eb05e73b1e5eb57488a3ce8953e681
vincentprofile <vincentccs93@gmail.com>
2021-03-12 20:33:00 +0800

    added shading brush

--- index.js
+++ index.js
@@ -19,20 +19,19 @@ function randomRGB(){
     return [r,g,b];
 }
 function changeDivBG(e){
-    // add 10% black to current randomed color
-    const divColor = e.target.style.backgroundColor;
-    console.log(divColor);
-    if (divColor == ''){
-        // add random color to square div
-        const randomColor = randomRGB();
-        e.target.style.backgroundColor = `rgba(${randomColor[0]},${randomColor[1]},
-            ${randomColor[2]})`;
+    // add random color to square div
+
+    // const randomColor = randomRGB();
+    // e.target.style.backgroundColor = `rgba(${randomColor[0]},${randomColor[1]},
+    //         ${randomColor[2]})`;
+
+    // add 10% black to current randomed color (by adjusting the opacity)
+    const currentDivOpacity = Number(e.target.style.opacity);
+    if (currentDivOpacity < 1){
+        e.target.style.backgroundColor = `rgba(0, 0, 0)`;
+        e.target.style.opacity = currentDivOpacity + 0.1;
     }
-    else{
-        // how can I add 10% black to current color?
-        if(divColor == divColor)
-            console.log("true");
-    }
+
 }
 
 const clearBtn = document.querySelector('.clearBtn');
